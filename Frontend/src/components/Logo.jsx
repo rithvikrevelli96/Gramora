b6469795b35c164e470f7d2d8b41b352a8f945f3
rithvikrevelli96 <rithvikrevelli96@gmail.com>
2026-02-24 01:38:58 +0530

    Remove unused web frontend and ios folders

--- Frontend/src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import logo from '../../public/logo.png'; // or use '../assets/logo.png' if you moved it
-
-const Logo = () => (
-  <img src={logo} alt="Gramora Logo" className="logo" />
-);
-
-export default Logo;
